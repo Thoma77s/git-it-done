e50d3816dd677a0c9ed23b5e2448babe97a95d9f
Thoma77s <thomassiffc3@gmail.com>
2022-01-05 14:58:58 -0500

    added language buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector('#repo-container');
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function(user) {
 var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -83,4 +84,28 @@ var displayRepos = function(repos, searchTerm) {
   
 };
 
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items,language);
+      });
+    } else {
+      alert('Error: GitHub User Not Found')
+    }
+  });
+
+};
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+var buttonClickHandler = function(event) {
+  var language = event.target.getAttribute("data-language");
+  if (language) {
+    getFeaturedRepos(language);
+  }
+  repoContainerEl.textContent = "";
+}
+languageButtonsEl.addEventListener("click", buttonClickHandler);
